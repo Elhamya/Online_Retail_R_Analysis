eb1c0275d90b0c3944e48e00a9f32fa5d2ca43f6
Elhamya <79582728+Elhamya@users.noreply.github.com>
2025-08-01 11:40:58 +1000

    Update online_retail_data_analysis.R

--- R script/online_retail_data_analysis.R
+++ R script/online_retail_data_analysis.R
@@ -1,17 +1,1 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "e3ec1d3f-aeab-4344-b957-0bdf07452293",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Import essential libraries for data analysis and visualization:\n",
-    "install.packages(c("readxl", "dplyr", "lubridate", "stringr"))\n",
-    "library(readxl)\n",
-    "library(dplyr)\n",
-    "library(lubridate)\n",
-    "library(stringr)\n"
-   ]
-  },
+asfasf
